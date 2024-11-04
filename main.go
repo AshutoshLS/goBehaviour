7e2b6bf3c1f20ec0006b734d9ef7da9cd7d3620e
AshutoshLS <ashutosh.karanam@lifesignals.com>
2024-11-04 11:56:45 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"goBehaviour/overflow"
+)
+
+func main() {
+	fmt.Println("=========================================")
+	fmt.Println("Integer overflow example")
+	overflow.Uint32overflow()
+	fmt.Println("=========================================")
+}
